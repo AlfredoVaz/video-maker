8f83a5612608a04cbbd744ff825b1cbcde6a1a89
Alfredo Vaz <alfredovazn@gmail.com>
2019-05-07 16:26:48 -0300

    Inicio robos de texto

---
+++ robots/text.js
@@ -0,0 +1,55 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/credentials.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2")
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content)
+        const wikipediaConent = wikipediaResponse.get()
+
+        content.sourcerContentOriginal = wikipediaConent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourcerContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if(line.trim().length === 0 || line.trim().startsWith('='))
+                    return false
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+
+        function removeDatesInParentheses(text) {
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        }
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+
+        sentences.forEach((sentences) => {
+            content.sentences.push({
+                text: sentences,
+                keywords: [],
+                images:[]
+            })
+        })
+    }
+}
+
+module.exports = robot;
